fb00b7c352e94376f05f1f79777f38ed1cfeae62
DVSY7 <gbytkdduf1004@naver.com>
2026-05-12 10:27:09 +0900

    Phase 3 완료 - 통합테스트 작성

---
+++ server/src/user/user.e2e.spec.ts
@@ -0,0 +1,60 @@
+import { INestApplication } from "@nestjs/common"
+import { Test, TestingModule } from "@nestjs/testing";
+import { TypeOrmModule } from "@nestjs/typeorm";
+import request from 'supertest';
+import { AppModule } from "../app.module";
+
+describe('User API(e2e)', ()=>{
+  let app: INestApplication;
+
+  beforeAll(async ()=>{
+    const moduleFixture: TestingModule = await Test.createTestingModule({
+      imports: [
+        TypeOrmModule.forRoot({
+          type: 'mysql',
+          host: 'localhost',
+          port: 3306,
+          username: 'root',
+          password: '1009',
+          database: 'mydb_test', // 테스트용 DB
+          autoLoadEntities: true,
+          synchronize: true,
+        }),
+        AppModule,
+      ],
+    }).compile();
+
+    app = moduleFixture.createNestApplication();
+    await app.init();
+  });
+
+  afterAll(async () =>{
+    await app.close();
+  });
+
+  it('POST /user -> 유저 생성', ()=>{
+    return request(app.getHttpServer())
+      .post('/user')
+      .send({ name: 'test'})
+      .expect(201)
+  })
+
+  it('GET /user -> 유저 목록 조회', ()=>{
+    return request(app.getHttpServer())
+      .get('/user')
+      .expect(200)
+  })
+
+  it('PATCH /user/:id -> 유저 수정', () => {
+    return request(app.getHttpServer())
+      .patch('/user/1')
+      .send({ name: 'updated'})
+      .expect(200)
+  })
+
+  it('DELETE /user/:id -> 유저 삭제', ()=>{
+    return request(app.getHttpServer())
+      .delete('/user/1')
+      .expect(200)
+  })
+})
