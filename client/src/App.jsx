2dbe525515f59f64d5caa11c7c57b4adf3417ed1
DVSY7 <gbytkdduf1004@naver.com>
2026-05-13 10:28:39 +0900

    fix: API_URL 환경변수로 분리

--- client/src/App.jsx
+++ client/src/App.jsx
@@ -1,7 +1,7 @@
 import { useEffect, useState } from "react";
 import axios from 'axios';
 
-const API = 'http://43.201.107.65:3000/user'
+const API = import.meta.env.VITE_API_URL || 'http://localhost:3000/user'
 
 export const App = () => {
   const [users, setUsers] = useState([])
