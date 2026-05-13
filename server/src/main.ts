876b8d81f959b22bf72fae16cf33bc4e4a3993de
DVSY7 <gbytkdduf1004@naver.com>
2026-05-13 11:37:28 +0900

    fix:CORS_ORIGIN runtime env로 수정

--- server/src/main.ts
+++ server/src/main.ts
@@ -5,7 +5,7 @@ async function bootstrap() {
   const app = await NestFactory.create(AppModule);
 
   app.enableCors({
-    origin: 'http://43.201.107.65:5173', // React 주소 허용
+    origin: process.env.CORS_ORIGIN || 'http://localhost:5173', // React 주소 허용
   });
 
   await app.listen(process.env.PORT ?? 3000);
